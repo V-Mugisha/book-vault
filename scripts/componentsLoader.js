9cb823b772eed997bb0f460ed4c476df1e253ec9
V-Mugisha <m.victor@alustudent.com>
2025-10-15 09:04:25 +0200

    Deleted unnecessary comments

--- scripts/componentsLoader.js
+++ scripts/componentsLoader.js
@@ -1,16 +1,3 @@
-
-// function loadComponent(componentId) {
-//     const targetElement = document.getElementById(componentId);
-//     const componentPath = `components/${componentId}.html`;
-//     fetch(componentPath)
-//         .then(response => response.text())
-//         .then(html => targetElement.innerHTML = html);
-// }
-
-// loadComponent('navigation');
-// loadComponent('features');
-
-// Component Loader with active link
 function loadComponent(componentId) {
     const targetElement = document.getElementById(componentId);
     if (!targetElement) {
